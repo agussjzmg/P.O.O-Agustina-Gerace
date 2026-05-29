0f5942b89e175879466ed87d813af53164854eed
agusjazgeracee <agustinajazmingerace2009@gmail.com>
2026-05-29 10:42:22 -0300

    Add files via upload

---
+++ Agustina Gerace Act14/punto3/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto3
+{
+    /*3. Competencia de Atletismo
+    Plantear una clase llamada Atleta que tenga como atributos privados el Nombre y el TiempoSegundos (tiempo que tardó en completar una carrera). Definir un constructor para ingresar ambos datos.
+    Luego, crear una clase llamada Carrera que administre 3 objetos de la clase Atleta como atributos de la clase.
+    Definir los siguientes métodos en la clase Carrera:
+    Un constructor que cargue los datos de los 3 atletas de la carrera.
+    Un método que imprima los nombres de los atletas junto con sus marcas de tiempo.
+    Un método que calcule e imprima el tiempo promedio de la carrera.
+    Un método que informe el nombre del atleta ganador de la carrera (aquel que completó la prueba en el menor tiempo).
+    Un método que muestre a los atletas que superaron el promedio.
+    */
+    internal class Atleta
+    {
+        private string nombre;
+
+        private double tiempoSegundos;
+
+        public Atleta()
+        {
+            string linea;
+            Console.Write("Ingrese el nombre del atleta: ");
+            nombre = Console.ReadLine();
+            Console.Write("Ingrese el tiempo en segundos: ");
+            linea = Console.ReadLine();
+            tiempoSegundos = double.Parse(linea);
+        }
+
+        public string NombreAtleta()
+        {
+            return nombre;
+        }
+
+        public double Tiempo()
+        {
+            return tiempoSegundos;
+        }
+    }
+
+    class Carrera
+    {
+        private Atleta[] atletas;
+
+        public Carrera()
+        {
+            atletas = new Atleta[3];
+            for (int i = 0; i < 3; i++)
+            {
+                atletas[i] = new Atleta();
+            }
+        }
+
+        public void Imprimir()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Atleta: " + atletas[i].NombreAtleta() + " su marca de tiempo es de: " + atletas[i].Tiempo() + " segundos");
+            }
+        }
+
+        public void CalcularPromedio()
+        {
+            double suma = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                suma = suma + atletas[i].Tiempo();
+            }
+            double promedio = suma / 3;
+            Console.WriteLine("El tiempo promedio de la carrera es: " + promedio );
+        }
+
+        public void Ganador()
+        {
+            double menor = atletas[0].Tiempo();
+            string nombreGanador = atletas[0].NombreAtleta();
+
+            for (int i = 1; i < 3; i++)
+            {
+                if (atletas[i].Tiempo() < menor)
+                {
+                    menor = atletas[i].Tiempo();
+                    nombreGanador = atletas[i].NombreAtleta();
+                }
+            }
+            Console.WriteLine("El ganador es: " + nombreGanador + " con un tiempo de " + menor );
+        }
+
+        public void SuperaronPromedio()
+        {
+            double suma = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                suma = suma + atletas[i].Tiempo();
+            
+            double promedio = suma / 3;
+
+            if (atletas[i].Tiempo() > promedio)
+                {
+                    Console.WriteLine("Los atletas que superaron el promedio son: " + atletas[i].NombreAtleta()  );
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Carrera c = new Carrera();
+            c.Imprimir();
+            c.CalcularPromedio();
+            c.Ganador();
+            c.SuperaronPromedio();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Agustina Gerace Act14/punto4/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto4
+{
+    /*4. Inventario de Sucursales 
+    Plantear una clase llamada Articulo que tenga como atributos privados el NombreArticulo, el Precio y el StockActual. El constructor de la clase debe cargar estos atributos por teclado.
+    Confeccionar otra clase llamada Sucursal que contenga un vector de 3 objetos de la clase Articulo. El constructor de Sucursal debe pedir el nombre o número de la sucursal y realizar la carga de los 3 artículos.
+    Implementar en la clase Sucursal los siguientes métodos:
+    Un método que imprima la lista de los artículos junto con su valor total en inventario (Precio * Stock).
+    Un método que busque y muestre el nombre del artículo más caro del local.
+    Un método que verifique e indique qué artículos de la sucursal requieren reposición urgente (aquellos cuyo stock sea menor o igual a 5 unidades).
+    */
+    class Articulo
+    {
+        private string nombre;
+        private double precio;
+        private int stock;
+
+        public Articulo()
+        {
+            string linea;
+            Console.Write("ingrese el nombre del articulo: ");
+            nombre = Console.ReadLine();
+            Console.Write("ingrese su precio: ");
+            linea = Console.ReadLine();
+            precio = double.Parse(linea);
+            Console.Write("ingrese el stock del articulo: ");
+            linea = Console.ReadLine();
+            stock = int.Parse(linea);
+        }
+        public string RetornarNombre()
+        {
+            return nombre;
+        }
+
+        public double RetornarPrecio()
+        {
+            return precio;
+        }
+
+        public int RetornarStock()
+        {
+            return stock;
+        }
+    }
+class Sucursal
+   {
+        private Articulo[] art;
+        private string nombreSucu;
+
+        public Sucursal()
+        {
+            Console.Write("Ingrese el nombre o numero de la sucursal: ");
+            nombreSucu = Console.ReadLine();
+
+            art = new Articulo[3];
+            for (int t = 0; t < 3; t++)
+            {
+                art[t] = new Articulo();
+            }
+        }
+        public void Imprimir()
+        {
+            Console.WriteLine("Lista de articulos en el inventario de: " + nombreSucu);
+
+            for (int t = 0; t < art.Length; t++)
+            {
+                double total = art[t].RetornarPrecio() * art[t].RetornarStock();
+
+                Console.WriteLine(
+                    art[t].RetornarNombre() +
+                    " valor total: " + total);
+            }
+        }
+        public void MasCaro()
+        {
+            double mayor = art[0].RetornarPrecio();
+            string nomMayor = art[0].RetornarNombre();
+
+            for (int t = 1; t < art.Length; t++)
+            {
+                if (art[t].RetornarPrecio() > mayor)
+                {
+                    mayor = art[t].RetornarPrecio();
+                    nomMayor = art[t].RetornarNombre();
+                }
+            }
+
+            Console.WriteLine("El articulo mas caro es: " + nomMayor);
+        }
+        public void RepoUrg()
+        {
+
+            for (int t = 0; t < art.Length; t++)
+            {
+                if (art[t].RetornarStock() <= 5)
+                {
+                    Console.WriteLine( "Articulo con reposición urgente: " + art[t].RetornarNombre());
+                }
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Sucursal s = new Sucursal();
+            s.Imprimir();
+            s.MasCaro();
+            s.RepoUrg();
+        }
+    }
+}
