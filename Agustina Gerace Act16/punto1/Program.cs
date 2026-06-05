30be304fcb549d21a37ef9e4d00919368a98fea5
agusjazgeracee <agustinajazmingerace2009@gmail.com>
2026-06-05 11:51:27 -0300

    Add files via upload

---
+++ Agustina Gerace Act16/punto1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace punto1
+{
+    /*1. Confeccionar una clase Persona que tenga como atributos el nombre y la
+    edad (definir las propiedades para poder acceder a dichos atributos). Definir
+    un método para imprimirlos. Plantear una segunda clase Empleado que
+    herede de la clase Persona. Añadir un atributo sueldo ( y su propiedad) y el
+    método para imprimir su sueldo. Definir un objeto de la clase Persona y
+    llamar a sus métodos y propiedades. También crear un objeto de la clase
+    Empleado y llamar a sus métodos y propiedades.*/
+    class Persona
+    {
+        protected string nombre;
+        protected int edad;
+        public string Nombre
+        {
+            set
+            {
+                nombre = value;
+            }
+            get
+            {
+                return nombre;
+            }
+        }
+
+        public int Edad
+        {
+            set
+            {
+                edad = value;
+            }
+            get
+            {
+                return edad;
+            }
+        }
+        public void Imprimir()
+        {
+            Console.WriteLine(Nombre + " con una edad de " + Edad);
+        }
+    }
+    class Empleado : Persona
+    {
+        protected float sueldo;
+        public float Sueldo
+        {
+            set
+            {
+                sueldo = value;
+            }
+            get
+            {
+                return sueldo;
+            }
+        }
+        public void ImprimirSueldo()
+        {
+            Console.WriteLine(Nombre + " tiene un sueldo de: " + Sueldo);
+        }
+
+        static void Main(string[] args)
+        {
+            Persona p = new Persona();
+            p.Nombre = "Agustina";
+            p.Edad = 26;
+            p.Imprimir();
+            Empleado e = new Empleado();
+            e.Nombre = "Thomas";
+            e.Edad = 27;
+            e.Sueldo = 1200000;
+            e.Imprimir();
+            e.ImprimirSueldo();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Agustina Gerace Act16/punto2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto2
+{
+    /*2. Plantear una clase Producto y otra clase Inventario.
+    La clase Producto debe tener como atributos privados el nombre, precio y
+    stock. Definir propiedades para acceder a estos atributos, asegurando que el
+    stock no pueda ser negativo y el precio sea mayor a cero.
+    La clase Inventario debe contener 3 objetos de la clase Producto. Definir un
+    método para mostrar todos los productos ordenados de menor a mayor en
+    base al precio, además, mostrar el producto más caro y más barato del
+    inventario.*/
+     class Producto
+    {
+        private string nombre;
+        private float precio;
+        private int stock;
+
+        public Producto()
+        {
+            string linea;
+            Console.Write("ingrese el producto: ");
+            nombre = Console.ReadLine();
+            Console.Write("ingrese el precio: ");
+            linea = Console.ReadLine();
+            precio = float.Parse(linea);
+            Console.Write("ingrese el stock del producto: ");
+            linea = Console.ReadLine();
+            stock = int.Parse(linea);
+
+            }
+           
+        static void Main(string[] args)
+        {
+        }
+    }
+}
